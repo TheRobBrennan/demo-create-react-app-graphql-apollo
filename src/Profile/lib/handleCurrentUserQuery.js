b58014ed650c777e0c623d49fdccf689319c9eee
Rob Brennan <rob@therobbrennan.com>
2019-02-12 20:09:59 -0800

    Now displaying a loading indicator when our request is pending

--- src/Profile/lib/handleCurrentUserQuery.js
+++ src/Profile/lib/handleCurrentUserQuery.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export const handleCurrentUserQuery = ({ data }) => {
+export const handleCurrentUserQuery = ({ data, loading }) => {
   const { viewer } = data
 
-  if (!viewer) return null
+  // Display a loading indicator if our query is pending
+  if (loading || !viewer) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div>{viewer.name} {viewer.login}</div> 
