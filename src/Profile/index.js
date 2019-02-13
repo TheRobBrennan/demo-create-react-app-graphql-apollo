e143609726d38b568f65956917ed96215c183026
Rob Brennan <rob@therobbrennan.com>
2019-02-12 19:52:32 -0800

    Crude GraphQL query to get details of the currently logged in user

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
-const Profile = () => <div>Profile</div>
+const GET_CURRENT_USER = gql`
+  {
+    viewer {
+      login
+      name
+    }
+  }
+`
+
+const Profile = () => (
+  <Query query={GET_CURRENT_USER}>
+    {() => <div>My Profile</div>}
+  </Query>
+)
 
 export default Profile
