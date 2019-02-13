2e560b32441a45f780566c24dc6b930e457a0e3d
Rob Brennan <rob@therobbrennan.com>
2019-02-12 20:07:09 -0800

    Added helpful comment

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -5,6 +5,7 @@ import { Query } from 'react-apollo'
 import { GET_CURRENT_USER } from '../graphql/queries/getCurrentUser'
 import { handleCurrentUserQuery } from './lib/handleCurrentUserQuery'
 
+// REMEMBER: The query will get executed as soon as this component renders
 const Profile = () => (
   <Query query={GET_CURRENT_USER}>
     {handleCurrentUserQuery}
