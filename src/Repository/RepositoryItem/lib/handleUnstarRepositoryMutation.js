61ed076245fb9404df64ea880b2e02bc0c086280
Rob Brennan <rob@therobbrennan.com>
2019-02-12 23:20:21 -0800

    Updated comment for handleUnstarRepositoryMutation

--- src/Repository/RepositoryItem/lib/handleUnstarRepositoryMutation.js
+++ src/Repository/RepositoryItem/lib/handleUnstarRepositoryMutation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from '../../../Button'
 
-// Note that the first argument - addStar - is the mutation itself and
+// Note that the first argument - removeStar - is the mutation itself and
 // it IS NOT THE MUTATION RESULT (like we saw in the Query component).
 //
 // We will use this function to trigger the mutation; later we will see
