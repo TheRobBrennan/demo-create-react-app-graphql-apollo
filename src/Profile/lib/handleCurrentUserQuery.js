f1df763018ec882459b4ae476d69ba2cd837a0b5
Rob Brennan <rob@therobbrennan.com>
2019-02-12 20:11:59 -0800

    Refactored Loading into its own component

--- src/Profile/lib/handleCurrentUserQuery.js
+++ src/Profile/lib/handleCurrentUserQuery.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import Loading from '../../Loading'
+
 export const handleCurrentUserQuery = ({ data, loading }) => {
   const { viewer } = data
 
   // Display a loading indicator if our query is pending
   if (loading || !viewer) {
-    return <div>Loading...</div>
+    return <Loading />
   }
 
   return (
