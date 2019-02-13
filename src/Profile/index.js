9ffbcf628862f8518e17b50ac128d79bf07054b9
Rob Brennan <rob@therobbrennan.com>
2019-02-12 20:40:12 -0800

    Using GET_REPOSITORIES_OF_CURRENT_USER query in Profile

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 // GraphQL
 import { Query } from 'react-apollo'
-import { GET_CURRENT_USER } from '../graphql/queries/getCurrentUser'
+import { GET_REPOSITORIES_OF_CURRENT_USER} from '../graphql/queries/getRepositoriesOfCurrentUser'
 import { handleCurrentUserQuery } from './lib/handleCurrentUserQuery'
 
 // REMEMBER: The query will get executed as soon as this component renders
 const Profile = () => (
-  <Query query={GET_CURRENT_USER}>
+  <Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
     {handleCurrentUserQuery}
   </Query>
 )
