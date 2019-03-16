2a7222bb0653285335a7525e2eb02aa58ef9570d
Rob Brennan <rob@therobbrennan.com>
2019-03-16 16:06:00 -0700

    Defined const to read a GraphQL fragment from a known Repository entity with the ID that matches our mutation result

--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Mutation } from 'react-apollo'
+import { REPOSITORY_FRAGMENT} from '../../graphql/fragments/repository'
 import { STAR_REPOSITORY } from '../../graphql/mutations/starRepository'
 import { handleStarRepositoryMutation } from './lib/handleStarRepositoryMutation'
 import { UNSTAR_REPOSITORY } from '../../graphql/mutations/unstarRepository'
@@ -9,7 +10,22 @@ import Link from '../../Link'
 import '../style.css'
 
 const updateAddStar = (client, { data: { addStar: { starrable: { id }}}}) => {
-  // Notice how we replaced mutationResult with the shape defined in the STAR_REPOSITORY mutation
+  // Apollo client's cache normalizes and stores queried data automatically.
+  // In REPOSITORY_FRAGMENT, we defined a repository fragment on the Repository entity.
+  // In this update function, our mutation result is giving us the ID of the repostitory,
+  // which means we can use the Apollo client readFragment method to read data from
+  // ANY NODE WE HAVE QUERIED PREVIOUSLY.
+  //
+  // See https://www.apollographql.com/docs/react/advanced/caching.html#readfragment for details.
+  const repository = client.readFragment({
+    id: `Repository:${id}`,
+    fragment: REPOSITORY_FRAGMENT,
+  })
+
+  // Update count of stargazers of repository
+
+  // Write repository back to cache
+
 }
 
 const RepositoryItem = ({
