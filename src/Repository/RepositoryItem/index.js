da7d0496624dcdfd64940d6beef76aedc85416be
Rob Brennan <rob@therobbrennan.com>
2019-03-16 16:27:49 -0700

    Refactored updateAddStar into its own lib

--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -1,45 +1,15 @@
 import React from 'react'
 import { Mutation } from 'react-apollo'
-import { REPOSITORY_FRAGMENT} from '../../graphql/fragments/repository'
+
 import { STAR_REPOSITORY } from '../../graphql/mutations/starRepository'
 import { handleStarRepositoryMutation } from './lib/handleStarRepositoryMutation'
+import { updateAddStar } from './lib/updateAddStar'
+
 import { UNSTAR_REPOSITORY } from '../../graphql/mutations/unstarRepository'
 import { handleUnstarRepositoryMutation } from './lib/handleUnstarRepositoryMutation'
 
 import Link from '../../Link'
 import '../style.css'
-
-const updateAddStar = (client, { data: { addStar: { starrable: { id }}}}) => {
-  // Apollo client's cache normalizes and stores queried data automatically.
-  // In REPOSITORY_FRAGMENT, we defined a repository fragment on the Repository entity.
-  // In this update function, our mutation result is giving us the ID of the repostitory,
-  // which means we can use the Apollo client readFragment method to read data from
-  // ANY NODE WE HAVE QUERIED PREVIOUSLY.
-  //
-  // See https://www.apollographql.com/docs/react/advanced/caching.html#readfragment for details.
-  const repository = client.readFragment({
-    id: `Repository:${id}`,
-    fragment: REPOSITORY_FRAGMENT,
-  })
-
-  // Update count of stargazers of repository incrementing the value
-  // contained within the shape of our repository fragment
-  const totalCount = repository.stargazers.totalCount + 1
-
-  // Write repository data back to cache
-  client.writeFragment({
-    id: `Repository:${id}`,
-    fragment: REPOSITORY_FRAGMENT,
-    data: {
-      ...repository,
-      stargazers: {
-        ...repository.stargazers,
-        totalCount,
-      }
-    }
-  })
-
-}
 
 const RepositoryItem = ({
   id,
