0d523489edd3b2a6ae5d7e359e46d7f08dc37cd2
Rob Brennan <rob@therobbrennan.com>
2019-03-16 15:43:39 -0700

    Defined a new GraphQL fragment REPOSITORY_FRAGMENT

--- src/graphql/queries/getRepositoriesOfCurrentUser.js
+++ src/graphql/queries/getRepositoriesOfCurrentUser.js
@@ -1,4 +1,5 @@
 import gql from 'graphql-tag'
+import { REPOSITORY_FRAGMENT } from '../fragments/repository'
 
 export const GET_REPOSITORIES_OF_CURRENT_USER = gql`
 {
@@ -9,28 +10,12 @@ export const GET_REPOSITORIES_OF_CURRENT_USER = gql`
     ) {
       edges {
         node {
-          id
-          name
-          url
-          descriptionHTML
-          primaryLanguage {
-            name
-          }
-          owner {
-            login
-            url
-          }
-          stargazers {
-            totalCount
-          }
-          viewerHasStarred
-          watchers {
-            totalCount
-          }
-          viewerSubscription
+          ...repository
         }
       }
     }
   }
 }
+
+${REPOSITORY_FRAGMENT}
 `
