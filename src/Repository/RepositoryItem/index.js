c2c619357ed7962a5f9b3a6d6c0a0b29df108cac
Rob Brennan <rob@therobbrennan.com>
2019-03-17 11:55:33 -0700

    Added ability to watch or unwatch a repo with an optimistic response in the mutation

--- src/Repository/RepositoryItem/index.js
+++ src/Repository/RepositoryItem/index.js
@@ -9,8 +9,14 @@ import { UNSTAR_REPOSITORY } from '../../graphql/mutations/unstarRepository'
 import { handleUnstarRepositoryMutation } from './lib/handleUnstarRepositoryMutation'
 import { updateRemoveStar } from './lib/updateRemoveStar'
 
+import { WATCH_REPOSITORY } from '../../graphql/mutations/watchRepository'
+import { VIEWER_SUBSCRIPTIONS } from './lib/viewerSubscriptions'
+import { isWatch } from './lib/isWatch'
+import { updateWatch } from './lib/updateWatch'
+
 import Link from '../../Link'
 import '../style.css'
+import Button from '../../Button';
 
 const RepositoryItem = ({
   id,
@@ -31,15 +37,53 @@ const RepositoryItem = ({
       </h2>
       <div>
         {!viewerHasStarred ? (
-          <Mutation mutation={STAR_REPOSITORY} variables={{ id }} update={updateAddStar}>
+          <Mutation
+            mutation={STAR_REPOSITORY}
+            variables={{ id }}
+            update={updateAddStar}
+          >
             {handleStarRepositoryMutation(stargazers)}
           </Mutation>
         ) : (
-          <Mutation mutation={UNSTAR_REPOSITORY} variables={{ id }} update={updateRemoveStar}>
+          <Mutation
+            mutation={UNSTAR_REPOSITORY}
+            variables={{ id }}
+            update={updateRemoveStar}
+          >
             {handleUnstarRepositoryMutation(stargazers)}
           </Mutation>
         )}
-        {/* Here comes your updateSubscription mutation */}
+
+        <Mutation
+          mutation={WATCH_REPOSITORY}
+          variables={{
+            id,
+            viewerSubscription: isWatch(viewerSubscription)
+              ? VIEWER_SUBSCRIPTIONS.UNSUBSCRIBED
+              : VIEWER_SUBSCRIPTIONS.SUBSCRIBED,
+          }}
+          optimisticResponse={{
+            updateSubscription: {
+              __typename: 'Mutation',
+              subscribable: {
+                __typename: 'Repository',
+                id,
+                viewerSubscription: isWatch(viewerSubscription)
+                  ? VIEWER_SUBSCRIPTIONS.UNSUBSCRIBED
+                  : VIEWER_SUBSCRIPTIONS.SUBSCRIBED
+              }
+            }
+          }}
+          update={updateWatch}
+        >
+          {(updateSubscription, { data, loading, error }) => (
+            <Button className="RepositoryItem-title-action"
+            onClick={updateSubscription}>
+              {watchers.totalCount}{' '}
+              {isWatch(viewerSubscription) ? 'Unwatch' : 'Watch'}
+            </Button>
+          )}
+        </Mutation>
       </div>
     </div>
 
