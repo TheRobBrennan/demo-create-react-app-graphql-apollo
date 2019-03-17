b1573f54934a50034f6a9e5ffa785c40b7a9eb67
Rob Brennan <rob@therobbrennan.com>
2019-03-17 12:31:56 -0700

    Updated RepositoryList to display a loading indicator

--- src/Repository/RepositoryList/index.js
+++ src/Repository/RepositoryList/index.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react'
 
+import Loading from '../../Loading'
 import RepositoryItem from '../RepositoryItem'
 import { updateQuery } from './lib/updateQuery'
 
 import '../style.css'
 
-const RepositoryList = ({ repositories, fetchMore }) => (
+const RepositoryList = ({ repositories, loading, fetchMore }) => (
   <Fragment>
     {repositories.edges.map(({ node }) => (
       <div key={node.id} className="RepositoryItem">
@@ -13,15 +14,24 @@ const RepositoryList = ({ repositories, fetchMore }) => (
       </div>
     ))}
 
-    {repositories.pageInfo.hasNextPage && (
-      <button type='button' onClick={() => fetchMore({
-        variables: {
-          cursor: repositories.pageInfo.endCursor,
-        },
-        updateQuery,
-      })}>
-        More Repositories
-      </button>
+    {loading ? (
+      <Loading />
+    ) : (
+      repositories.pageInfo.hasNextPage && (
+        <button
+          type="button"
+          onClick={() =>
+            fetchMore({
+              variables: {
+                cursor: repositories.pageInfo.endCursor,
+              },
+              updateQuery,
+            })
+          }
+        >
+          More Repositories
+        </button>
+      )
     )}
   </Fragment>
 )
